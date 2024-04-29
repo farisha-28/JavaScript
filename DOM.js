a913cdf9672b43accf02ae959e77300b7b2b4c76
Farisha Hussain <hfarisha06@gmail.com>
2024-04-29 12:55:10 +0600

    DOM file updated

--- DOM.js
+++ DOM.js
@@ -1,3 +1,7 @@
 console.dir(document);
 console.dir(window.document);
 console.dir(document.body);
+console.dir(document.getElementById("topics"));
+console.log("Heading topic -- ",document.getElementById("topics"));
+console.dir(document.getElementsByClassName("examples"));
+console.log("Heading example -- ",document.getElementsByClassName("examples"));
