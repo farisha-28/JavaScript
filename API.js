fcc5b6f5651bec582ff9ec616943a53c3b4a3ded
Farisha Hussain <hfarisha06@gmail.com>
2024-04-29 09:36:56 +0600

    API File added

--- API.js
+++ API.js
@@ -4,3 +4,26 @@
 //              used for interacting with APIs to retrieve and send
 //              data asynchronously over the web.
 //              fetch(url, {options})
+console.log("one");
+console.log("two");
+function hello() {
+  console.log("hello");
+}
+setTimeout(hello, 2000); // asyn: we set when this function will run(after 2s / 2000ms)
+
+setTimeout(() => {
+  console.log("api", 4000); //asynchronus
+});
+console.log("three");
+console.log("four");
+
+// callback function
+
+
+
+//fetch api
+
+
+
+
+
--- function_method.js
+++ function_method.js
@@ -44,16 +44,36 @@ let newArr  = nums.map( (val) => {
 console.log("Map array " + newArr);
 
 // Filter method to filter some values
+
 let fil = [3,2,1,4,6,7,5,8];
 let evenArray = fil.filter( (val) => {
-    return val % 2 === 0;  // val % 2!== 0
+    return val % 2 === 0;  // val % 2!== 0 // val > 99
 });
 console.log("Filter array: "+evenArray)
 
 // Reduce function array: perfomrs some operations and reduces the array to a single value. it returns the single value
+
 let red = [1,2,3,4,5];
 const output = red.reduce( (res, current) => {
     return res + current;
 });
 console.log(output);
+
+// Print maximum number using reduce
+
+let maxi = [1,2,3,4,5,100];
+const largest = maxi.reduce( (res, current) => {
+    return res > current ? res : current;       // is largest ?, then print res, : otherwise print current.
+});
+console.log("Maximum value: "+largest);
+
+//take a number from user and create an array of numbers from 1 to n.
+// and make sum of 
+
+let take = prompt("Enter a number: ");
+let natural = [];
+for(let i=1; i<=take; i++){
+    natural[i-1] = i;
+}
+console.log("1 to n: "+ natural);
  
